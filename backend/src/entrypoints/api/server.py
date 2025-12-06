2e18a07ccd918d2f1b54abcc6b5bd2ce63c7d9d8
Stepan Filonov <stepanfilonov@gmail.com>
2025-12-06 21:01:13 +0800

    workis

--- backend/src/entrypoints/api/server.py
+++ backend/src/entrypoints/api/server.py
@@ -4,15 +4,17 @@ from fastapi import FastAPI, HTTPException
 from loguru import logger
 from starlette.requests import Request
 
+from core.tools import list_outstanding_bills, pay_outstanding_bill, open_account
 from infrastructure.models import ToolType
-from infrastructure.repositories import get_call_by_phone_number
+from infrastructure.repositories import get_call_by_phone_number, close_account
 from core.tools.transfer_money import transfer_money_between_own_accounts
 from entrypoints.api.serializers import (
     ServerWebhookPayload,
     ToolCallsMessage,
     ToolCallResult,
     ToolCallsResponse,
-    TransferMoneyOwnAccountsToolCallParameters,
+    TransferMoneyOwnAccountsToolCallParameters, PayBillToolCallParameters, OpenAccountToolCallParameters,
+    CloseAccountToolCallParameters,
 )
 from settings import settings
 
@@ -75,6 +77,39 @@ async def webhook_handler(
                 **tool_calls_msg.tool_calls[0].function.arguments
             ),
         )
+    elif tool_name == ToolType.LIST_BILLS:
+        result = await list_outstanding_bills(
+            call_id=call_id,
+            tool_invocation_id=tool_invocation_id,
+            user_id=call.user_id,
+        )
+    elif tool_name == ToolType.PAY_BILL:
+        result = await pay_outstanding_bill(
+            call_id=call_id,
+            tool_invocation_id=tool_invocation_id,
+            user_id=call.user_id,
+            tool_parameters=PayBillToolCallParameters(
+                **tool_calls_msg.tool_calls[0].function.arguments
+            )
+        )
+    elif tool_name == ToolType.OPEN_ACCOUNT:
+        result = await open_account(
+            call_id=call_id,
+            tool_invocation_id=tool_invocation_id,
+            user_id=call.user_id,
+            tool_parameters=OpenAccountToolCallParameters(
+                **tool_calls_msg.tool_calls[0].function.arguments
+            ),
+        )
+    elif tool_name == ToolType.CLOSE_ACCOUNT:
+        result = await close_account(
+            call_id=call_id,
+            tool_invocation_id=tool_invocation_id,
+            user_id=call.user_id,
+            tool_parameters=CloseAccountToolCallParameters(
+                **tool_calls_msg.tool_calls[0].function.arguments
+            ),
+        )
 
     return ToolCallsResponse(
         results=[
@@ -84,8 +119,6 @@ async def webhook_handler(
             )
         ]
     )
-
-    return {}
 
 
 if __name__ == "__main__":
